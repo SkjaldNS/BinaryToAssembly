60a5e0c43d3a072d0717397b934560f7c7e6d5f4
Nikolaj Schæbel <s220471@dtu.dk>
2024-04-09 18:25:28 +0200

    Implemented Binary to Assembly converter

--- src/FileUtils.java
+++ src/FileUtils.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class FileUtils {
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileUtils {
+
+    static String[] loadFile(String filename) throws IOException {
+        String resultSet = "";
+        String[] result;
+        File file = new java.io.File(filename);
+        if(file.exists()) {
+            try {
+                Scanner scanner = new java.util.Scanner(file);
+                while (scanner.hasNextLine()) {
+                    resultSet += scanner.nextLine() + "\n";
+                }
+                result = resultSet.split("\n");
+            }
+            catch (Exception e) {
+                throw new IOException("Error reading file");
+            }
+
+        } else {
+            throw new IOException("File not found");
+        }
+        return result;
+    }
+
+    static void saveFile(String filename, String[] data) throws IOException {
+        File file = new java.io.File(filename);
+        try {
+            java.io.PrintWriter writer = new java.io.PrintWriter(file);
+            for (String line : data) {
+                writer.println(line);
+            }
+            writer.close();
+        }
+        catch (Exception e) {
+            throw new IOException("Error writing file");
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("Enter 1 for binary instruct, enter 2 to load from a file: ");
+            try {
+                int choice = scanner.nextInt();
+                if (choice == 1) {
+                    System.out.println("Enter binary instruction: ");
+                    String binary = scanner.next();
+                    if(!Binary.validateBinary(binary)) {
+                        System.out.println("Invalid binary");
+                        continue;
+                    }
+                    System.out.println(Binary.convertBinaryToAssembly(binary));
+                } else if (choice == 2) {
+                    System.out.println("Enter filename: ");
+                    String filename = scanner.next();
+                    String[] data = FileUtils.loadFile(filename);
+                    if(!Binary.validateBinary(data)) {
+                        System.out.println("Invalid binary");
+                        continue;
+                    }
+                    String resultSet = "";
+                    for (String line : data) {
+                        resultSet += Binary.convertBinaryToAssembly(line) + "\n";
+                        System.out.println(Binary.convertBinaryToAssembly(line));
+                    }
+                    System.out.println("Enter filename to save: ");
+                    filename = scanner.next();
+                    FileUtils.saveFile(filename, resultSet.split("\n"));
+                } else {
+                    System.out.println("Invalid choice");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input");
+                scanner.next();
+            }
+        }
+    }
 }
